e8855cdc3d246dfcee82ce105bbc71b2efced769
mukul71 <mukul71@gmail.com>
2023-09-10 23:29:19 +0600

    Master

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,1 @@
-//import 'package:flutter/material.dart';
-
-//We will discuss about flotter code flow here whch is the primary
-// or basic structure of any flutter code flow. The code flow is as below:
-//1. Main ()
-//2. MyApp ()
-//3. Material ()
-//4. Activity Page
-//5. Scafold which has two sections
-//A. App Bar
-//B. Body
-//1. Main ()
-//2. MyApp ()
-//3. Material ()
-//4. Activity Page
-//5. Scafold which has two sections
-//A. App Bar
-//B. Body
+//master
