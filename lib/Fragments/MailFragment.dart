e2f6e4efff16a005473b3fcc766754d81aab981d
Mukul Rahman <mukul71@gmail.com>
2023-09-19 16:25:37 +0600

    Fragments Description Created

---
+++ lib/Fragments/MailFragment.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class MailFragment extends StatelessWidget {
+  const MailFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Mail Fragment'),
+      ),
+    );
+  }
+}
---
+++ lib/Fragments/WalletFragment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class WalletFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Wallet Fragment'),
+      ),
+    );
+  }
+}
