06064e74c1f230ea0a35f7da49b18198246cce50
Mukul Rahman <mukul71@gmail.com>
2023-09-19 17:59:02 +0600

    JSON Array created with image

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,18 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  var myItems = [
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Hasan"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Kalam"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Shafik"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Hamza"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Tuhin"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Amzad"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Nazmul"},
+    {"image": "https://rabbil.com/files/mernS.png", "title": "Ali"},
+  ];
 
   @override
   Widget build(BuildContext context) {
