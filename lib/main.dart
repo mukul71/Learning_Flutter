a7961234ed571285a37dd1ae02fbf0970c9e1530
Mukul Rahman <mukul71@gmail.com>
2023-09-20 17:41:45 +0600

    Creating Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -15,61 +15,12 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home: Data Pass'),
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => ProductPage()));
-        },
-        child: Text('Go to ProductPage'),
-      ),
-    );
-  }
-}
 
-class ProductPage extends StatelessWidget {
-  const ProductPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Page'),
-      ),
-      body: ListView.builder(
-        itemCount: 50,
-        itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          ProductDetailPage(productName: index.toString())));
-            },
-            title: Text(index.toString()),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class ProductDetailPage extends StatelessWidget {
-  final productName;
-  const ProductDetailPage({super.key, required this.productName});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail Page'),
-      ),
-      body: Center(
-        child: Text(productName),
+        title: Text('Theme Data'),
       ),
     );
   }
