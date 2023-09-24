7837e908daaaa8ab385d84d5f75d0cb1ddc6cbfc
Mukul Rahman <mukul71@gmail.com>
2023-09-24 22:43:15 +0600

    Button style added

--- lib/Style.dart
+++ lib/Style.dart
@@ -4,12 +4,18 @@ InputDecoration AppInputs(label) {
   return InputDecoration(
     contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
     filled: true,
-    fillColor: Colors.green,
+    fillColor: Colors.white,
     border: OutlineInputBorder(),
     labelText: label,
   );
 }
 
 TextStyle HeadStyle() {
-  return TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
+  return TextStyle(fontSize: 20, fontWeight: FontWeight.w800);
+}
+
+ButtonStyle appButtonStyle() {
+  return ElevatedButton.styleFrom(
+    backgroundColor: Colors.green,
+  );
 }
