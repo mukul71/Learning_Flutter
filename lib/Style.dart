f7060e06a15a33a93aaf3c4f54f8e4e777591a88
Mukul Rahman <mukul71@gmail.com>
2023-09-24 19:51:04 +0600

    InputDecoration function added

---
+++ lib/Style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+InputDecoration AppInputs() {
+  return InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+      fillColor: Colors.green);
+}
