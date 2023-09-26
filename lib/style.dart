92b9a3b963e885f0e2ad57381be5b7c8e058233d
Mukul Rahman <mukul71@gmail.com>
2023-09-26 16:42:49 +0600

    ButtonStyle added

--- lib/style.dart
+++ lib/style.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-InputDecoration appInputDecoration() {
+InputDecoration appInputDecoration(label) {
   return InputDecoration(
     contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+    border: OutlineInputBorder(),
+    labelText: label,
+  );
+}
+
+ButtonStyle appButtonStyle() {
+  return ElevatedButton.styleFrom(
+    padding: EdgeInsets.all(22),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
   );
 }
