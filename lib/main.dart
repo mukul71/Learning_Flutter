8e7bd7ccac39d2cc47833d08d294fa1391c2aa70
Mukul Rahman <mukul71@gmail.com>
2023-09-28 15:37:24 +0600

    Rows added

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,51 @@ class HomePage extends StatefulWidget {
 class HomeView extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Calculator'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('1')),
+              ElevatedButton(onPressed: () {}, child: Text('2')),
+              ElevatedButton(onPressed: () {}, child: Text('3')),
+              ElevatedButton(onPressed: () {}, child: Text('+')),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('4')),
+              ElevatedButton(onPressed: () {}, child: Text('5')),
+              ElevatedButton(onPressed: () {}, child: Text('6')),
+              ElevatedButton(onPressed: () {}, child: Text('-')),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('7')),
+              ElevatedButton(onPressed: () {}, child: Text('8')),
+              ElevatedButton(onPressed: () {}, child: Text('9')),
+              ElevatedButton(onPressed: () {}, child: Text('/')),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('0')),
+              ElevatedButton(onPressed: () {}, child: Text('=')),
+              ElevatedButton(onPressed: () {}, child: Text('%')),
+              ElevatedButton(onPressed: () {}, child: Text('*')),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
