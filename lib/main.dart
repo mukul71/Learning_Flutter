f892ed2acc3e2831c644acb211621cc1739427a0
Mukul Rahman <mukul71@users.noreply.github.com>
2023-10-14 16:46:56 +0600

    Update main.dart
    Fibonacci

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
-//master
+void main() {
+  int n = 15;
+  int first = 1;
+  int second = 1;
+  if (n >= 1) {
+    print('1');
+  }
+  if (n >= 2) {
+    print('1');
+  }
+  for (var i = 3; i < n; i++) {
+    int result = first + second;
+    print(result);
+    first = second;
+    second = result;
+  }
+}//master
